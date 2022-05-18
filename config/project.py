1b2d16a3c12670c2ae71f5af5b5152ad5803e8e2
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:55 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-elk"
-project_long_description = "Demos for the elk stack"
-project_year_started = 2019
-project_description = "Demos for the elk stack"
+description_short = "Demos for the elk stack"
