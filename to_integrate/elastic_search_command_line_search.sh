b7a396155806ede1d4d188732639403638103893
Mark Veltzer <mark.veltzer@gmail.com>
2022-06-19 05:37:06 +0300

    more stuff

---
+++ to_integrate/elastic_search_command_line_search.sh
@@ -0,0 +1,10 @@
+curl -X GET "localhost:9200/_search?pretty" -H 'Content-Type: application/json' -d'
+{
+  "query": {
+    "query_string": {
+      "query": "Veltzer",
+      "default_field": "Surname"
+    }
+  }
+}
+'
