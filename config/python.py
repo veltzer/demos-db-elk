d280b9495970cd7044d656aa653a7b8b415708a4
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 01:52:12 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,12 +1,15 @@
-config_requires = []
-dev_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = [
     "elasticsearch",
 ]
-install_requires = []
-make_requires = [
+install_requires: List[str] = []
+make_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
-test_requires = []
+test_requires: List[str] = []
 requires = config_requires + install_requires + make_requires + test_requires
