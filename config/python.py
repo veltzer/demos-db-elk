71e090ca4f99211df37e6f4bad7817fd3a40bf18
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:16:04 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,10 +1,11 @@
 """ python deps for this project """
 
+
+import config.shared
+
+
 install_requires: list[str] = [
     "elasticsearch",
 ]
-build_requires: list[str] = [
-    "pymakehelper",
-    "pydmt",
-]
+build_requires: list[str] = config.shared.BUILD
 requires = install_requires + build_requires
